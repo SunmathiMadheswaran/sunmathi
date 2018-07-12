20b7a0450c219320180947722988d923d6a5f452
SunmathiMadheswaran <41140883+SunmathiMadheswaran@users.noreply.github.com>
2018-07-12 14:24:45 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,8 @@
+alph=input("")
+if((alph>='a'and alph<='z')or(alph>='A' and alph<='Z')):
+	if alph in ['a','e','i','o','u','A','E','I','O','U']:
+		print("Vowel")
+	else:
+		print("Consonant")
+else:
+	print("inValid")
