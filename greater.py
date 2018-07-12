40008fad43b2af68444aa9a74d652c4854e2196c
SunmathiMadheswaran <41140883+SunmathiMadheswaran@users.noreply.github.com>
2018-07-12 15:15:43 +0530

    Update greater.py

--- greater.py
+++ greater.py
@@ -1,9 +1,13 @@
-a=int(input(""))
-b=int(input(""))
-c=int(input(""))
-if(a>b and a>c):
+import sys
+L=sys.stdin.read()
+L=L.split()
+L=[int(i) for i in L]
+a=L[0]
+b=L[1]
+c=L[2]
+if a>b and a>c:
 	print(a)
-elif(b>c):
+elif b>c:
 	print(b)
 else:
 	print(c)
